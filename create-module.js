b0622ee4ad7c1df5af98c66ff914ce81ee57ff00
Andre Fuentes <afuentes@cielo.com.br>
2019-08-02 13:01:14 -0300

    corrigindo bugs

--- create-module.js
+++ create-module.js
@@ -42,26 +42,26 @@ function criarModulo() {
   fs.mkdirSync(`./${moduleName}/modules`, {recursive: true});
   fs.mkdirSync(`./${moduleName}/services`, {recursive: true});
 
-  //fs.appendFileSync(`${moduleName}/README.md`, '', function (err) { if (err) throw err });
-  //fs.appendFileSync(`${moduleName}/${moduleNameCamel}.controller.js`, moduleControllerTemplate(moduleName, moduleNameCamel), function (err) { if (err) throw err });
-  //fs.appendFileSync(`${moduleName}/${moduleName}.html`, moduleMainHTMLTemplate(moduleName), function (err) { if (err) throw err });
-  fs.appendFileSync(`${moduleName}/${moduleName}.module.js`, moduleTemplate(moduleName, moduleNameCamel), function (err) { if (err) throw err });
-  fs.appendFileSync(`${moduleName}/${moduleName}.component.html`, moduleComponentHTMLTemplate(moduleName, componentList), function (err) { if (err) throw err });
-  fs.appendFileSync(`${moduleName}/${moduleName}.component.js`, moduleComponentJSTemplate(moduleName, moduleNameCamel, PATH), function (err) { if (err) throw err });
-  fs.appendFileSync(`${moduleName}/${moduleName}.component.scss`, moduleComponentSASSTemplate(componentList), function (err) { if (err) throw err });
-  fs.appendFileSync(`${moduleName}/services/${moduleName}.service.js`, moduleServiceTemplate(moduleName, moduleNameCamel), function (err) { if (err) throw err });
-  fs.appendFileSync(`${moduleName}/assets/images/.gitkeep`, '', function (err) { if (err) throw err });
-  fs.appendFileSync(`${moduleName}/assets/stylesheets/.gitkeep`, '', function (err) { if (err) throw err });
-  fs.appendFileSync(`${moduleName}/_files.txt`, log(moduleName, moduleNameCamel, componentList, PATH), function (err) { if (err) throw err });
+  //fs.writeFileSync(`${moduleName}/README.md`, '', function (err) { if (err) throw err });
+  //fs.writeFileSync(`${moduleName}/${moduleNameCamel}.controller.js`, moduleControllerTemplate(moduleName, moduleNameCamel), function (err) { if (err) throw err });
+  //fs.writeFileSync(`${moduleName}/${moduleName}.html`, moduleMainHTMLTemplate(moduleName), function (err) { if (err) throw err });
+  fs.writeFileSync(`${moduleName}/${moduleName}.module.js`, moduleTemplate(moduleName, moduleNameCamel), function (err) { if (err) throw err });
+  fs.writeFileSync(`${moduleName}/${moduleName}.component.html`, moduleComponentHTMLTemplate(moduleName, componentList), function (err) { if (err) throw err });
+  fs.writeFileSync(`${moduleName}/${moduleName}.component.js`, moduleComponentJSTemplate(moduleName, moduleNameCamel, PATH), function (err) { if (err) throw err });
+  fs.writeFileSync(`${moduleName}/${moduleName}.component.scss`, moduleComponentSASSTemplate(componentList), function (err) { if (err) throw err });
+  fs.writeFileSync(`${moduleName}/services/${moduleName}.service.js`, moduleServiceTemplate(moduleName, moduleNameCamel), function (err) { if (err) throw err });
+  fs.writeFileSync(`${moduleName}/assets/images/.gitkeep`, '', function (err) { if (err) throw err });
+  fs.writeFileSync(`${moduleName}/assets/stylesheets/.gitkeep`, '', function (err) { if (err) throw err });
+  fs.writeFileSync(`${moduleName}/_files.txt`, log(moduleName, moduleNameCamel, componentList, PATH), function (err) { if (err) throw err });
 }
 
 function criarComponentes() {
   componentList.forEach(component => {
     let componentCamel = _.camelCase(component);
     fs.mkdirSync(`./${moduleName}/components/${component}`, {recursive: true});
-    fs.appendFileSync(`${moduleName}/components/${component}/_${component}.component.scss`, '', function (err) { if (err) throw err });
-    fs.appendFileSync(`${moduleName}/components/${component}/${component}.component.html`, componentHTMLTemplate(component), function (err) { if (err) throw err });
-    fs.appendFileSync(`${moduleName}/components/${component}/${component}.component.js`, componentJSTemplate(moduleName, moduleNameCamel, component, componentCamel, PATH), function (err) { if (err) throw err });
+    fs.writeFileSync(`${moduleName}/components/${component}/_${component}.component.scss`, '', function (err) { if (err) throw err });
+    fs.writeFileSync(`${moduleName}/components/${component}/${component}.component.html`, componentHTMLTemplate(component), function (err) { if (err) throw err });
+    fs.writeFileSync(`${moduleName}/components/${component}/${component}.component.js`, componentJSTemplate(moduleName, moduleNameCamel, component, componentCamel, PATH), function (err) { if (err) throw err });
   });
 }
 
